01bb9af529aa01e319077d439beef871aacb17a0
AgentKazy <kazygaming@outlook.pt>
2023-03-08 09:58:47 +0000

    139. A Short Introduction to JavaScript

--- Lessons/09-Omnifood-Optimizations/js/script.js
+++ Lessons/09-Omnifood-Optimizations/js/script.js
@@ -1,3 +1,20 @@
+// console.log("Hello world!");
+
+// const myName = "Bruno Martins";
+// const h1 = document.querySelector(".heading-primary");
+// console.log(myName);
+// console.log(h1);
+
+// h1.addEventListener("click", (e) => {
+//   h1.textContent = myName;
+//   h1.style.backgroundColor = "red";
+//   h1.style.padding = "5rem";
+// });
+
+const yearEl = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+console.log(currentYear);
+yearEl.textContent = currentYear;
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
